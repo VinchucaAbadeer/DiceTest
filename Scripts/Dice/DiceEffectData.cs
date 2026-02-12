c0634050714649c77976dadbb84fc6300b801cbf
VinchucaAbadeer <142061985+VinchucaAbadeer@users.noreply.github.com>
2026-02-12 16:21:33 -0300

    Delete Scripts/Dice directory

--- Scripts/Dice/DiceEffectData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class DiceEffectData : ScriptableObject {
-
-    public string effectName;
-    public abstract void ApplyEffect (BattleContext context, RollResult result);
-}
